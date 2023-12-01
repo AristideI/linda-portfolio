fff7907105edc92ceba50213e8fc3a5d5748698b
AristideI <a.isingizwe@alustudent.com>
2023-12-01 12:51:03 +0200

    latest feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.html"],
   theme: {
-    extend: {},
+    theme: {
+      screens: {
+        "2xl": { max: "1535px" },
+        xl: { max: "1279px" },
+        lg: { max: "1023px" },
+        md: { max: "767px" },
+        sm: { max: "639px" },
+      },
+    },
+    extend: {
+      colors: {
+        green1: "#C5D926",
+        green2: "#A6CD8E",
+        green3: "#26432F",
+        gray1: "#3D3D3D",
+        gray2: "#c3c3c3",
+        bg: "#FBFEE1",
+      },
+    },
   },
   plugins: [],
 };
